c6fb3afa09ca3c7bd2a4c4710adbb32125a3ba69
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-02-25 16:18:31 +0100

    updated route cron

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -9,7 +9,6 @@ import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 export const maxDuration = 10; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
-
 export async function GET() {
     try {
         connectToDB();
